f0a6b4b8d5033455a5645cc8097d8291b37fc44c
harsh8823 <toooharsh@gmail.com>
2025-12-31 12:26:47 +0530

    time to burn tree

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,110 @@
 import java.util.*;
 
-public class Main{
+class Node {
 
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String str=scn.nextLine();
-        valueOfExpression(str);
+    int data;
+    Node left;
+    Node right;
 
-    }
-    public static boolean isOperator(char c){
-        return (c=='%' || c=='*' || c=='-' || c=='+' || c=='/');
-    }
-    public static int calculate(int opr1,int opr2,char c){
-        if(c=='+') return opr1+opr2;
-        if(c=='-') return opr1-opr2;
-        if(c=='*') return opr1*opr2;
-        if(c=='/') return opr1/opr2;
-        return opr1%opr2;
-    }
-    public static void solve(String str){
-        Stack<Integer> st=new Stack<>();
-        for(int i=str.length()-1;i>=0;i--){
-            char c=str.charAt(i);
-            if(isOperator(c)){
-                int val1=st.pop();
-                int val2=st.pop();
-                int ans=calculate(val1,val2,c);
-                st.push(ans);
-            }
-            else{
-                st.push(c-'0');
-            }
-        }
-
-        System.out.println(st.peek());
-    }
-    public static void prefixToInfix(String str){
-        Stack<String> st=new Stack<>();
-        for(int i=str.length()-1;i>=0;i--){
-            char c=str.charAt(i);
-            if(isOperator(c)){
-                String val1=st.pop();
-                String val2=st.pop();
-                st.push("("+val1+c+val2+")");
-            }
-            else{
-                st.push(c+"");
-            }
-        }
-        System.out.println(st.peek());
-    }
-    public static void prefixToPostfix(String str){
-        Stack<String> st=new Stack<>();
-        for(int i=str.length()-1;i>=0;i--){
-            char c=str.charAt(i);
-            if(isOperator(c)){
-                String val1=st.pop();
-                String val2=st.pop();
-                st.push(val1+val2+c);
-            }
-            else{
-                st.push(c+"");
-            }
-        }
-        System.out.println(st.peek());
-    }
-    public static void valueOfExpression(String str)
-    {
-        // your code here
-        solve(str);
-        prefixToInfix(str);
-        prefixToPostfix(str);
-
+    public Node(int data) {
+        this.data = data;
     }
 }
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.next();
+        int k = sc.nextInt();
+        sc.close();
+        Node tree = levelOrderCreateTree(arr);
+        System.out.println(timeToBurnTree(tree, k));
+    }
+
+    public static Node findStartNode(Node root,int start){
+        if(root==null) return null;
+        if(root.data==start) return root;
+
+        Node leftAns=findStartNode(root.left,start);
+        if(leftAns!=null) return leftAns;
+
+        return findStartNode(root.right,start);
+    }
+
+    public static void getChildParentMap(HashMap<Node,Node> mp,Node root){
+        if(root==null) return;
+
+        if(root.left!=null) mp.put(root.left,root);
+        if(root.right!=null) mp.put(root.right,root);
+
+        getChildParentMap(mp,root.left);
+        getChildParentMap(mp,root.right);
+    }
+
+
+    public static int timeToBurnTree(Node root, int start) {
+        if(root==null) return 0;
+        //   Write your code here
+        HashMap<Node,Node> mp=new HashMap<>();
+        getChildParentMap(mp,root);
+
+        Node s=findStartNode(root,start);
+        if(s==null) return 0;
+        Queue<Node> q=new ArrayDeque<>();
+        q.add(s);
+        HashSet<Node> st=new HashSet<>();
+        int time=-1;
+
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Node curr=q.poll();
+                st.add(curr);
+                if(curr.left!=null && !st.contains(curr.left)) q.add(curr.left);
+                if(curr.right!=null && !st.contains(curr.right)) q.add(curr.right);
+
+                if(mp.containsKey(curr)){
+                    Node parent=mp.get(curr);
+                    if(!st.contains(parent)) q.add(parent);
+                }
+            }
+            time++;
+        }
+        return time;
+    }
+
+    static void createTree(Node node, int i, String[] arr) {
+        if (node != null) {
+            if (2 * i + 1 < arr.length) {
+                if (arr[2 * i + 1].equals("null")) {
+                    node.left = null;
+                } else {
+                    node.left = new Node(Integer.parseInt(arr[2 * i + 1]));
+                }
+                createTree(node.left, 2 * i + 1, arr);
+            }
+            if (2 * i + 2 < arr.length) {
+                if (arr[2 * i + 2].equals("null")) {
+                    node.right = null;
+                } else {
+                    node.right = new Node(Integer.parseInt((arr[2 * i + 2])));
+                }
+                createTree(node.right, 2 * i + 2, arr);
+            }
+        }
+    }
+
+    static Node levelOrderCreateTree(String[] arr) {
+        if (arr.length == 0) return null;
+        Node head = new Node(
+                Integer.parseInt(arr[0])
+        );
+        createTree(head, 0, arr);
+        return head;
+    }
+}
+
+
